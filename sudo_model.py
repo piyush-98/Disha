eb64f88b6d7eaac833637b15583f9a34c7c2b8c1
Himani Negi <negi.himani11@gmail.com>
2019-01-12 19:47:14 +0530

    Update sudo_model.py

--- sudo_model.py
+++ sudo_model.py
@@ -1,5 +1,26 @@
-from scipy.misc import imread
-from scipy.misc import imresize
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.models import model_from_json
+import os
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+filepath="C:\\Users\\Deii\\Downloads\\manus"
+
+os.chdir(filepath)
+json_file = open('modell.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+filepath2="C:\\Users\\Deii\\Downloads\\manus\\model3.h5"
+loaded_model.load_weights(filepath2)
+print("Loaded model from disk")
+
+
 
 
 
@@ -8,15 +29,5 @@ from scipy.misc import imresize
 #im = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
 
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
-
-
-loaded_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-score = loaded_model.predict_classes(im, verbose=1)
 
